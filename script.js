173dff4364c3442c4025e71a21d41efb95f1ce25
Carlos Willian <carlodwillian@gmail.com>
2021-04-20 15:54:08 -0300

    Upgrade

--- script.js
+++ script.js
@@ -47,3 +47,23 @@ var swiper = new Swiper('.swiper-container', {
     },
   });
 
+// SEARCH FILTER
+const search = document.querySelectorAll(".search");
+const productName = document.querySelectorAll(".product-details h2");
+
+// A BETTER WAY TO FILTER THROUGH THE PRODUCTS
+/*search.addEventListener("keyup", filterProducts);
+
+
+function filterProducts(e) {
+    const text = e.target.value.toLowerCase();
+    console.log(productName[0]);
+    productName.forEach(function(product) {
+        const item = product.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            product.parentElement.parentElement.style.display = "block"
+        } else {
+            product.parentElement.parentElement.style.display = "none"
+        }
+    })
+}*/
